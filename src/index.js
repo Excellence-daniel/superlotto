ab1b41ad6a0ac7217d8e66cc2a8929d7e18c92e7
Shalom <oyeniranexcellenced@gmail.com>
2019-03-19 21:26:12 +0100

    Redefined the class to be called on start of the app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './pages/index';
+import Routerr from './pages/router/router';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<Routerr/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
