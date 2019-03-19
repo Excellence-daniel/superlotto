05633f5d50f2749cfc7ccabc28ce1c7f26000ea7
Shalom <oyeniranexcellenced@gmail.com>
2019-03-19 21:26:57 +0100

    Added a code to be displayed on the index page

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <img alt = "SuperLotto Img" src="" />
+            </div>
+        )
+    }
+}
