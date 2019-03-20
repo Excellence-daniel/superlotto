a7eb9dc88aeab112ee469ecd467c8bdcaa02180d
Shalom <oyeniranexcellenced@gmail.com>
2019-03-20 18:23:30 +0100

    Validating emails and checking if the user's email is verified to complete the login action

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,8 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React, { Component } from 'react';
 import Header from './header';
-import { fireAuth } from '../config';
+import { fireAuth, fireStore } from '../config';
+import validator from 'validator';
 
 export default class Login extends Component {
     constructor(props) {
@@ -13,53 +15,70 @@ export default class Login extends Component {
     }
 
     handleEmailInput = (e) => {
-        if (e.target.value){
-            this.setState({email : e.target.value});
+        if (e.target.value) {
+            this.setState({ email: e.target.value });
         }
     }
 
     handlePassWordInput = (e) => {
-        if (e.target.value){
-            this.setState({password : e.target.value});
+        if (e.target.value) {
+            this.setState({ password: e.target.value });
         }
     }
 
     handleLogin = async () => {
-        const {email, password} = this.state;
-        if (email === '' || password === ''){
+        const { email, password } = this.state;
+        if (email === '' || password === '') {
             alert('Complete all fields')
         } else {
-            await fireAuth.auth().signInWithEmailAndPassword(email, password)
-            console.log('logged in')
-            var user = await fireAuth.auth().currentUser;
-            console.log(user)
+            if (validator.isEmail(email)){
+                try {
+                    await fireAuth.auth().signInWithEmailAndPassword(email, password)
+                    const getUserQuery = await fireStore.collection('Users').where('Email', '==', email).get();
+                    console.log(getUserQuery);
+                    if (getUserQuery.docs[0].EmailVerified){
+                        var user = await fireAuth.auth().currentUser;
+                        localStorage.setItem('UserLoggedIn' , true);
+                        console.log(user);
+                    } else {
+                        await fireAuth.auth().signOut();
+                        localStorage.removeItem('UserLoggedIn');
+                    }
+                }
+                catch (err) {
+                    console.log(err.message);
+                    alert(err.message);
+                }
+            } else {
+                alert('Email Format Wrong! Enter Valid Email')
+            }
         }
     }
     render() {
         return (
             <div>
                 <Header />
-                <div className="container">
+                <div className="container-fluid mt-6">
                     <div className="row">
-                        <div className="col-12 col-md-6 mt-4">
+                        <div className="col-12 col-md-6 mt-5">
                             <center><h2> Login Form </h2></center>
                             <div className="row">
                                 <p className="col-12">
                                     <label> Email Address </label>
-                                    <input onChange = {this.handleEmailInput} type="email" placeholder="you@yourmail.com" />
+                                    <input onChange={this.handleEmailInput} type="email" placeholder="you@yourmail.com" />
                                 </p>
 
                                 <p className="col-12">
                                     <label> Password </label>
-                                    <input onChange = {this.handlePassWordInput} type="password" />
+                                    <input onChange={this.handlePassWordInput} type="password" />
                                 </p>
                                 <p className="col-12">
-                                    <button onClick = {this.handleLogin} className="btn btn-info btn-block"> Login </button>
+                                    <button onClick={this.handleLogin} className="btn btn-info btn-block"> Login </button>
                                 </p>
                             </div>
                         </div>
                         <div className="col-12 col-md-6">
-                            Picture
+                            <img src="https://superlottong.com/Content/assets/superlotto/img/web_banner1.jpg" style = {{width : '100%'}} className = "img-fluid"/>
                     </div>
                     </div>
                 </div>
