ea543e35badcaebbb6ba4bae846e53fa51bb5103
Shalom <oyeniranexcellenced@gmail.com>
2019-03-20 15:52:46 +0100

    Alowed fireauth to create a new user with email and password also add data relating to the email in firestore collection

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Header from './header';
-import {fireAuth , database} from '../config/index';
+import { fireAuth, fireStore } from '../config/index';
 
 export default class SignUp extends Component {
     constructor(props) {
@@ -12,56 +12,56 @@ export default class SignUp extends Component {
             address: '',
             phonenumber: '',
             password: '',
-            dateofbirth : ''
+            dateofbirth: ''
         }
     }
 
     handleFirstNameInput = (e) => {
-        if (e.target.value){
-            this.setState({firstname : e.target.value.trim()});
+        if (e.target.value) {
+            this.setState({ firstname: e.target.value.trim() });
         }
     }
 
     handleLastNameInput = (e) => {
-        if (e.target.value){
-            this.setState({lastname : e.target.value.trim()});
+        if (e.target.value) {
+            this.setState({ lastname: e.target.value.trim() });
         }
     }
 
     handleEmailInput = (e) => {
-        if (e.target.value){
-            this.setState({email : e.target.value.trim()});
+        if (e.target.value) {
+            this.setState({ email: e.target.value.trim() });
         }
     }
 
     handleAddressInput = (e) => {
-        if (e.target.value){
-            this.setState({address : e.target.value.trim()});
+        if (e.target.value) {
+            this.setState({ address: e.target.value.trim() });
         }
     }
 
     handlePhoneNumberInput = (e) => {
-        if (e.target.value){
-            this.setState({phonenumber : e.target.value.trim()});
+        if (e.target.value) {
+            this.setState({ phonenumber: e.target.value.trim() });
         }
     }
 
     handleDateOfBirthInput = (e) => {
-        if (e.target.value){
-            this.setState({dateofbirth : e.target.value.trim()});
+        if (e.target.value) {
+            this.setState({ dateofbirth: e.target.value.trim() });
         }
     }
 
     handlePasswordInput = (e) => {
-        if (e.target.value){
-            this.setState({password : e.target.value.trim()});
+        if (e.target.value) {
+            this.setState({ password: e.target.value.trim() });
         }
     }
 
     handleConfirmPassword = (e) => {
         var signupBtn = document.getElementById('signupBtn')
-        if (e.target.value){
-            if (e.target.value !== this.state.password){
+        if (e.target.value) {
+            if (e.target.value !== this.state.password) {
                 console.log("Wrong Password");
                 signupBtn.disabled = true;
             } else {
@@ -71,10 +71,33 @@ export default class SignUp extends Component {
     }
 
     signUp = async () => {
-        const {firstname, lastname, email, password, dateofbirth, address, phonenumber} = this.state;
-        var currentUser = await fireAuth.auth().currentUser;
-        console.log(currentUser);
-        console.log('Can sign up now');
+        const { firstname, lastname, email, password, dateofbirth, address, phonenumber } = this.state;
+        if (firstname === '' || lastname === '' || email === '' || password === '' || dateofbirth === '' || address === '' || phonenumber === '') {
+            console.log('Complete all fields');
+        } else {
+            try {
+                // const getUserQuery = await fireStore.collection('Users').where('Email', '===', email).get();
+                // if (getUserQuery.empty) {
+                    await fireAuth.auth().createUserWithEmailAndPassword(email, password)
+                    await fireStore.collection('Users').add({
+                        Name: firstname + ' ' + lastname,
+                        Email: email,
+                        BirthDay: dateofbirth,
+                        Address: address,
+                        PhoneNumber: phonenumber,
+                        Password: password,
+                        UserToken: ''
+                    })
+                    console.log('Created')
+                // } else {
+                //     console.log('A user exists with this email. Please use a unique email address')
+                // }
+            }
+            catch (err) {
+                console.log(err.message);
+                alert('Error.' + err.message);
+            }
+        }
     }
 
     render() {
@@ -86,43 +109,43 @@ export default class SignUp extends Component {
                     <div className="row">
                         <p className="col-12 col-md-6">
                             <label> First Name </label>
-                            <input onChange = {this.handleFirstNameInput} type="text" />
+                            <input onChange={this.handleFirstNameInput} type="text" />
                         </p>
 
                         <p className="col-12 col-md-6">
                             <label> Last Name </label>
-                            <input onChange = {this.handleLastNameInput} type="text" />
+                            <input onChange={this.handleLastNameInput} type="text" />
                         </p>
                     </div>
 
                     <div className="row">
                         <p className="col-12 col-md-6">
                             <label> Address </label>
-                            <input onChange = {this.handleAddressInput} type="text" />
+                            <input onChange={this.handleAddressInput} type="text" />
                         </p>
 
                         <p className="col-12 col-md-6">
                             <label> Email </label>
-                            <input onChange = {this.handleEmailInput} type="email" />
+                            <input onChange={this.handleEmailInput} type="email" />
                         </p>
                     </div>
 
                     <div className="row">
                         <p className="col-12 col-md-6">
                             <label> Phone Number </label>
-                            <input onChange = {this.handlePhoneNumberInput} type="text" />
+                            <input onChange={this.handlePhoneNumberInput} type="text" />
                         </p>
 
                         <p className="col-12 col-md-6">
                             <label> Date of Birth </label>
-                            <input onChange = {this.handleDateOfBirthInput} type="date" />
+                            <input onChange={this.handleDateOfBirthInput} type="date" />
                         </p>
                     </div>
 
                     <div className="row">
                         <p className="col-12 col-md-6">
                             <label> Password </label>
-                            <input onChange = {this.handlePasswordInput} type="password" />
+                            <input onChange={this.handlePasswordInput} type="password" />
 
                             <p>
                                 <b> Password Strength </b>
@@ -134,7 +157,7 @@ export default class SignUp extends Component {
 
                         <p className="col-12 col-md-6">
                             <label> Confirm Password </label>
-                            <input onChange = {this.handleConfirmPassword} type="password" />
+                            <input onChange={this.handleConfirmPassword} type="password" />
                         </p>
                     </div>
 
@@ -146,7 +169,7 @@ export default class SignUp extends Component {
                             </label>
                         </p>
                     </div>
-                    <button className="btn btn-success btn-block" id = "signupBtn" onClick = {this.signUp}> SIGN UP </button>
+                    <button className="btn btn-success btn-block" id="signupBtn" onClick={this.signUp}> SIGN UP </button>
 
                 </div>
             </div>
