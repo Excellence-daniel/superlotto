1b226fcb347276611cd72311fb9f360b31ff8001
Shalom <oyeniranexcellenced@gmail.com>
2019-03-20 17:15:01 +0100

    Created the fields for taking data for login

--- src/pages/login.js
+++ src/pages/login.js
@@ -5,25 +5,39 @@ export default class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            email : '', 
-            password : '', 
-            redirect : false
+            email: '',
+            password: '',
+            redirect: false
         }
     }
 
-    render(){
+    render() {
         return (
-            <div> 
+            <div>
                 <Header />
-                <div className = "container">
-                <div className = "row">
-                    <div className = "col-12 col-md-6">
-                        Login
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-md-6 mt-4">
+                            <center><h2> Login Form </h2></center>
+                            <div className="row">
+                                <p className="col-12">
+                                    <label> Email Address </label>
+                                    <input type="email" placeholder="you@yourmail.com" />
+                                </p>
+
+                                <p className="col-12">
+                                    <label> Password </label>
+                                    <input type="password" />
+                                </p>
+                                <p className="col-12">
+                                    <button className="btn btn-info btn-block"> Login </button>
+                                </p>
+                            </div>
+                        </div>
+                        <div className="col-12 col-md-6">
+                            Picture
                     </div>
-                    <div className = "col-12 col-md-6">
-                    Picture
                     </div>
-                </div>
                 </div>
             </div>
         )
