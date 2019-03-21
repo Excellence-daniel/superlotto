277c08dfc35c8186c8dcff08cc2cf7a6f1275ff1
Shalom <oyeniranexcellenced@gmail.com>
2019-03-21 14:35:49 +0100

    Added images and routes to respective games

--- src/pages/games.js
+++ src/pages/games.js
@@ -1,1 +1,36 @@
-import
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Header from './header';
+
+export default class Games extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <div className="container-fluid">
+                    <div className="row">
+                    <div className="col-12 col-md-4 frame zoomin">
+                        <Link to='/games/lottoball'>
+                                <h3> Lotto Balls </h3>
+                                <img src = "https://superlottong.com/Content/assets/superlotto/img/live-games/lucky-5.jpg" alt="Lotto Balls" className="img-fluid" />
+                        </Link>
+                        </div>
+                        <div className="col-12 col-md-4 frame zoomin">
+                        <Link to='/games/lottoball'>
+                                <h3> Game 2 </h3>
+                                <img src = "https://superlottong.com/Content/assets/superlotto/img/live-games/lucky-6.jpg" alt="Lotto Balls" className="img-fluid" />
+                        </Link>
+                        </div>
+                        <div className="col-12 col-md-4 frame zoomin">
+                        <Link to='/games/lottoball'>
+                                <h3> Game 3 </h3>
+                                <img src = "https://superlottong.com/Content/assets/superlotto/img/live-games/lucky-7.jpg" alt="Lotto Balls" className="img-fluid" />
+                        </Link>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+}
