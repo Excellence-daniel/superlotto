c073486056ad6d4de1d79746c44caeddb7cd3480
Shalom <oyeniranexcellenced@gmail.com>
2019-03-22 17:59:11 +0100

    Added a componentDidMount function to check firebase collection user if user with the token in the urlis verified, if not, verify and alert the user. And Close the window

--- src/pages/confirmEmail.js
+++ src/pages/confirmEmail.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
+import {parse} from 'query-string';
+import {fireStore} from '../config/index';
 
 export default class ConfirmEmail extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            redirect: false
+        }
+    }
+    componentDidMount = async () => {
+        const getURL = parse(this.props.location.search);
+        const userToken = getURL.UT;
+        let userDocID;
+        if (userToken){
+            const getUser = await fireStore.collection('Users').where('UserToken', '==', userToken).get();
+            const isEmailVerified = getUser.docs[0].data().EmailVerified;
+            userDocID = getUser.docs[0].id;
+            if (isEmailVerified){
+                alert('Email Verified already.')
+            } else {
+                await fireStore.collection('Users').doc(userDocID).update({
+                    EmailVerified : true
+                });
+                alert('Email Verification Successful');
+                this.window.close();
+            }
+        }
+        // console.log(getURL);
+    }
+
     render() {
-        if (this.state.redirect){
-            return <Redirect to = "/login"/>
+        if (this.state.redirect) {
+            return <Redirect to="/login" />
         }
         return (
             <div>
