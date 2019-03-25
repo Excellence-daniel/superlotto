07e3775195765a39da4000781cb10723309c8197
Shalom <oyeniranexcellenced@gmail.com>
2019-03-25 11:13:16 +0100

    The file renders a header and a coming soon text

---
+++ src/pages/results.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Header from './header';
+
+export default class Result extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <span className = "mt-4">
+                    <center><h2> Coming Soon... </h2></center>
+                </span>
+            </div>
+        )
+    }
+}
